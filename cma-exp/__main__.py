aaf5153c3eb0903467b79f9f178938ecdbeae50d
FMGS666 <b00794799@essec.edu>
2023-12-07 10:55:44 +0100

    Adding option whether to evaluate the origin as first point or not

--- cma-exp/__main__.py
+++ cma-exp/__main__.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         sigma0 = arguments.sigma0,
         cma_options = cma_options,
         noisy_settings = arguments.noisy_settings,
-        evaluate_origin = arguments.evaluate_origin
+        evaluate_origin = arguments.evaluate_origin,
         **cma_kwargs
     )
     benchmark.set_num_threads(
